d42d90835d59f73a6ad8c4d8f3aa9d8b6cc69c9a
jkgarber <joshuakgarber@outlook.com>
2025-05-18 23:12:43 +0200

    blueprints

---
+++ incontext/conversations.py
@@ -0,0 +1,95 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from incontext.auth import login_required
+from incontext.db import get_db
+
+bp = Blueprint('conversations', __name__, url_prefix='/conversations')
+
+@bp.route('/')
+@login_required
+def index():
+    db = get_db()
+    agents = db.execute(
+        'SELECT c.id, name, created, creator_id, username'
+        ' FROM conversations c JOIN users u ON c.creator_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('conversations/index.html', conversations=conversations)
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        name = request.form['name']
+        error = None
+
+        if not name:
+            error = 'Name is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO conversations (name, creator_id)'
+                ' VALUES (?, ?)',
+                (name, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('conversations.index'))
+
+    return render_template('conversations/create.html')
+
+def get_conversation(id, check_creator=True):
+    conversation = get_db().execute(
+        'SELECT c.id, name, created, creator_id, username'
+        ' FROM conversations c JOIN users u ON c.creator_id = u.id'
+        ' WHERE c.id = ?',
+        (id,)
+    ).fetchone()
+
+    if conversation is None:
+        abort(404, f"Conversation id {id} doesn't exist.")
+
+    if check_creator and conversation['creator_id'] != g.user['id']:
+        abort(403) # 403 means Forbidden. 401 means "Unauthorized" but you redirect to the login page instead of returning that status.
+
+    return conversation
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    conversation = get_conversation(id)
+
+    if request.method == 'POST':
+        name = request.form['name']
+        error = None
+
+        if not name:
+            error = 'Name is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE conversations SET name = ?'
+                ' WHERE id = ?',
+                (name, id)
+            )
+            db.commit()
+            return redirect(url_for('conversations.index'))
+    
+    return render_template('conversations/update.html', conversation=conversation)
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_conversation(id)
+    db = get_db()
+    db.execute('DELETE FROM conversations WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('conversations.index'))
---
+++ incontext/messages.py
@@ -0,0 +1,93 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from incontext.auth import login_required
+from incontext.db import get_db
+
+from openai import OpenAI
+
+bp = Blueprint('messages', __name__, url_prefix='/messages')
+
+
+def get_messages(conversation_id, check_creator=True):
+    messages = get_db().execute(
+        'SELECT m.id, m.content, m.human, m.created, c.creator_id'
+        'FROM messages m'
+        'JOIN conversations c'
+        'ON m.conversation_id = c.id'
+        'WHERE c.id = ?',
+        (conversation_id,)
+    ).fetchall()
+
+    if messages is None:
+        abort(404, f"Messages for conversation with id {id} don't exist.")
+
+    if check_creator and messages[0]['creator_id'] != g.user['id']:
+        abort(403) # 403 means Forbidden. 401 means "Unauthorized" but you redirect to the login page instead of returning that status.
+
+    return messages
+
+@bp.route('/<int:conversation_id>', methods=('GET'))
+@login_required
+def conversation_messages(id):
+    messages = get_messages(conversation_id)
+    return messages
+
+
+# def get_agent_response(cid):
+#     messages = get_messages(conversation_id)
+#     conversation_history = []
+#     for message in messages:
+#         human = message['human']
+#         if human == 1:
+#             role = 'user'
+#         elif human == 0:
+#             role = 'assistant'
+#         content = message['content']
+#         conversation_history.append(dict(role=role, content=content))
+# 
+#     client = OpenAI()
+#     try:
+#         response = client.responses.create(
+#             model='gpt-4.1-mini',
+#             input=conversation_history
+#         )
+#         return dict(success=True, content=response.output_text)
+#     except Exception as e:
+#         return dict(success=False, content=f'Error: {str(e)}')
+    
+
+@bp.route('/<int:conversation_id>/add', methods=('POST'))
+@login_required
+def add_message():
+    message_content = request.form['content']
+    error = None
+
+    if not content:
+        error = 'Message can\'t be empty.'
+
+    if error is not None:
+        flash(error)
+    else:
+        db = get_db()
+        db.execute(
+            'INSERT INTO messages (conversation_id, content, human)'
+            'VALUES (?, ?, ?)',
+            (conversation_id, message_content, 1,)
+        )
+        db.commit()
+        return '', 200        
+        # agent_response = get_agent_response(conversation_id)
+        # if agent_response['success']:
+        #     db = get_db()
+        #     db.execute(
+        #         'INSERT INTO messages (conversation_id, content, human)'
+        #         'VALUES (?, ?, ?)',
+        #         (conversation_id, agent_response['content'], 0,)
+        #     )
+        #     db.commit()
+        #     return agent_response['content']
+        # elif not agent_response['success']:
+        #     return 'The OpenAI API returned an error.'
